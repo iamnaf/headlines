c7f88b3b46eaa557f82030f9befc92514dd73a02
iamnaf <nafsulaiman@gmail.com>
2021-10-11 23:57:30 +0100

    add headlines.py

---
+++ headlines.py
@@ -0,0 +1,41 @@
+import feedparser
+from flask import Flask
+
+app = Flask(__name__)
+
+# BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn': 'http://rss.cnn.com/rss/edition.rss',
+             'fox': 'http://feeds.foxnews.com/latest',
+             'iol': 'http://www.iol.co.za/cmlink/1.640'}
+
+@app.route("/")
+@app.route("/bbc")
+def bbc():
+    return get_news('bbc')
+
+@app.route("/cnn")
+def cnn():
+    return get_news('cnn')
+
+@app.route("/iol")
+def iol():
+    return get_news('iol')
+
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article = feed['entries'][0]
+    return """
+    <html>
+        <body>
+            <h1>{0} Headlines</h1>
+            <b>{1}</b> <br>
+            <i>{2}</i> <br>
+            <p>{3}</p> <br>
+        </body>
+    <html>
+    """.format(publication.title(), first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
