260d31523df955335abac0cc313371baf22560e0
iamnaf <nafsulaiman@gmail.com>
2021-10-12 16:21:11 +0100

    add home.html

--- headlines.py
+++ headlines.py
@@ -1,5 +1,7 @@
 import feedparser
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
@@ -11,31 +13,28 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-@app.route("/bbc")
-def bbc():
-    return get_news('bbc')
+# @app.route("/bbc")
+# def bbc():
+#     return get_news('bbc')
 
-@app.route("/cnn")
-def cnn():
-    return get_news('cnn')
+# @app.route("/cnn")
+# def cnn():
+#     return get_news('cnn')
 
-@app.route("/iol")
-def iol():
-    return get_news('iol')
+# @app.route("/iol")
+# def iol():
+#     return get_news('iol')
 
-def get_news(publication):
+# @app.route("/<publication>")
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "bbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    first_article = feed['entries'][0]
-    return """
-    <html>
-        <body>
-            <h1>{0} Headlines</h1>
-            <b>{1}</b> <br>
-            <i>{2}</i> <br>
-            <p>{3}</p> <br>
-        </body>
-    <html>
-    """.format(publication.title(), first_article.get("title"), first_article.get("published"), first_article.get("summary"))
+    return render_template("home.html", articles=feed['entries'])
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
