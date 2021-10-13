f06ee23d74c8cc7f9c5bb206ce99937361e284d2
iamnaf <nafsulaiman@gmail.com>
2021-10-13 06:17:58 +0100

    Add get method in flask requests

--- headlines.py
+++ headlines.py
@@ -13,19 +13,6 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-# @app.route("/bbc")
-# def bbc():
-#     return get_news('bbc')
-
-# @app.route("/cnn")
-# def cnn():
-#     return get_news('cnn')
-
-# @app.route("/iol")
-# def iol():
-#     return get_news('iol')
-
-# @app.route("/<publication>")
 def get_news():
     query = request.args.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
